7598be8ed85e271abf6dba04e6fb9f5ab0806710
Hamlet Gordillo <hamletfg@gmail.com>
2023-12-01 14:14:14 -0700

    Adding loops for user logins

--- test/pageobjects/swagLogin.js
+++ test/pageobjects/swagLogin.js
@@ -4,34 +4,42 @@ import Page from './page.js';
 
 class LoginPage extends Page {
 
-    get inputUsername () {
-        return $('#user-name');
-    }
+    // Testing logins that should not work and return an error.
+    errorUsers = ['nonExisting_user', 'locked_out']
 
-    get inputPassword () {
-        return $('#password');
-    }
+    // Testing logins that should work and login. 
+    goodUsers = ['standard_user', 'problem_user', 'performance_glitch_user',
+    'error_user', ]
 
-    get loginButton () {
-        return $('#login-button');
-    }
+    get inputUsername () { return $('#user-name'); }
+    get inputPassword () { return $('#password'); }
+    get loginButton () { return $('#login-button'); }
+    get swagLabsHeader () { return $('.app_logo'); }
+    get loginError () { return $('[data-test="error"]'); }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+    /* This bit of automation code helps with entering user name
+    and password along with clicking the login button */
     async login (username, password) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.loginButton.click();
+        await this.inputUsername.setValue(username)
+        await this.inputPassword.setValue(password)
+        await this.loginButton.click()
     }
 
-    get swagLabsHeader () {
-        return $('.app_logo');
+    /* Here we're doing a loop to enter the array of names that
+    should return a login error */
+    async errorUsers () {
+        for (let i = 0; i < this.errorUsers.length; i++) {
+            await expect(this.swagLabsHeader).toExist()
+            await this.login(this.errorUsers[i], 'bad_password')
+            await expect(this.loginError).toHaveTextContaining('Username and password do not match any user in this service')
+            await this.login(this.errorNames[i], 'secret_sauce')
+            await expect(this.errorBox).toExist()
+        }
     }
 
-    get loginError () {
-        return $('[data-test="error"]');
+    // Now the loop for the goodUsers
+    async goodUsers () {
+        await expect(this.swagLabsHeader).toExist()
     }
 
     /**
